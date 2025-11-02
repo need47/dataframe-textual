002e0463028a2620b612e21ac81e8d575712a593
Tiejun Cheng <need47@gmail.com>
2025-11-02 15:09:28 -0500

    fix errors after renaming package

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Entry point for DataFrameViewer."""
 
-from dataframe_viewer.__main__ import main
+from dataframe_textual.__main__ import main
 
 if __name__ == "__main__":
     main()
