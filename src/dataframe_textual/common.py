caf628491dc9791c267e20ee05bc560db4ec996a
Cheng, Tiejun <chengt2@ncbi.nlm.nih.gov>
2025-12-09 10:51:55 -0500

    fix: update error message to suggest truncating ragged lines

--- src/dataframe_textual/common.py
+++ src/dataframe_textual/common.py
@@ -596,7 +596,7 @@ def handle_compute_error(
 
     # Schema mismatch error
     if "found more fields than defined in 'Schema'" in err_msg:
-        print(f"Input might be malformed:\n{err_msg}.\nTry again with `-E` to ignore errors", file=sys.stderr)
+        print(f"Input might be malformed:\n{err_msg}.\nTry again with `-t` to truncate ragged lines", file=sys.stderr)
         sys.exit(1)
 
     # ComputeError: could not parse `n.a. as of 04.01.022` as `dtype` i64 at column 'PubChemCID' (column number 16)
