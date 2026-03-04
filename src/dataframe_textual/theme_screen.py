b37e7082f5088974e9e06facf43ce47d7289a694
Cheng, Tiejun <chengt2@ncbi.nlm.nih.gov>
2026-03-04 07:41:17 -0500

    refactor: simplify theme management in ThemeScreen

--- src/dataframe_textual/theme_screen.py
+++ src/dataframe_textual/theme_screen.py
@@ -35,8 +35,7 @@ class ThemeScreen(ModalScreen):
     def __init__(self) -> None:
         super().__init__()
         self.themes = list(BUILTIN_THEMES.keys())
-        self.original_theme = ""
-        self._confirmed = False
+        self.original_theme = self.app.theme
 
     def compose(self) -> ComposeResult:
         with Container(id="theme-container") as container:
@@ -44,28 +43,22 @@ class ThemeScreen(ModalScreen):
             yield OptionList(*self.themes, id="theme-list")
 
     def on_mount(self) -> None:
-        self.original_theme = self.app.theme
         options = self.query_one(OptionList)
         if self.original_theme in self.themes:
             options.highlighted = self.themes.index(self.original_theme)
         options.focus()
 
     def _set_theme(self, theme: str) -> None:
-        if theme in BUILTIN_THEMES:
+        if theme in self.themes:
             self.app.theme = theme
 
     def on_option_list_option_highlighted(self, event: OptionList.OptionHighlighted) -> None:
         self._set_theme(str(event.option.prompt))
 
     def on_option_list_option_selected(self, event: OptionList.OptionSelected) -> None:
-        self._confirmed = True
         self._set_theme(str(event.option.prompt))
         self.app.pop_screen()
 
     def action_cancel(self) -> None:
         self.app.theme = self.original_theme
         self.app.pop_screen()
-
-    def on_unmount(self) -> None:
-        if not self._confirmed:
-            self.app.theme = self.original_theme
